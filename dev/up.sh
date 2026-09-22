851e17115702b202370b3bde08220d83b26e0d8f
Eitan Ostashinski <eitanoid.os@gmail.com>
2026-09-22 20:21:39 +0100

    added kuberay operator

--- dev/up.sh
+++ dev/up.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
 kind create cluster --config dev/kind.yaml
+
+helm repo add kuberay https://ray-project.github.io/kuberay-helm/
+helm repo update
+kubectl create namespace ray-system
+helm install kuberay-operator kuberay/kuberay-operator --version 1.7.0 -n ray-system
